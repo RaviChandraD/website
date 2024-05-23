6dc43f35f14832889a3467f5c08b5ecd7b316349
Ravi Chandra <dravichandra40@gmail.com>
2024-05-22 23:01:40 -0700

    push updated files

--- app.py
+++ app.py
@@ -1,32 +1,42 @@
-import os
-
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+from flask import Flask, request, render_template, redirect, url_for
+import pyodbc
 
 app = Flask(__name__)
 
+# Database configuration
+server = 'server0210.database.windows.net'
+database = 'server0210'
+username = 'CloudSAe7252128'
+password = 'rC$02101998rC'
+driver = '{ODBC Driver 17 for SQL Server}'
+
+# Function to connect to the database
+def get_db_connection():
+    conn = pyodbc.connect('DRIVER=' + driver +
+                          ';SERVER=' + server +
+                          ';PORT=1433;DATABASE=' + database +
+                          ';UID=' + username +
+                          ';PWD=' + password)
+    return conn
 
 @app.route('/')
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    email = request.form['email']
+    age = request.form['age']
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO Users (name, email, age) VALUES (?, ?, ?)", (name, email, age))
+    conn.commit()
+    cursor.close()
+    conn.close()
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
-   app.run()
+    app.run(debug=True)
